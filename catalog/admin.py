1580569d3fff1cee1f954d2de9cb223a8e422790
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-07-07 02:41:15 +0000

    Register catalog models in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Author, Book, Borrowing, Genre
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "date_of_birth")
+    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "inventory", "available_copies")
+    list_filter = ("genres", "author")
+    search_fields = ("title", "author__first_name", "author__last_name")
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = ("book", "borrower", "borrowed_at", "due_date", "returned_at")
+    list_filter = ("returned_at", "due_date")
+    search_fields = ("book__title", "borrower__username")
